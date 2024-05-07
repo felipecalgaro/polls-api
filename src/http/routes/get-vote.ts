8323f1c878055bfc9fb321428d40f968d0fd9437
Felipe <pipecalgaro@gmail.com>
2024-05-07 20:14:16 -0300

    feat: implement getVote route

---
+++ src/http/routes/get-vote.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { prisma } from "../../lib/prisma";
+import { FastifyInstance } from "fastify";
+
+export async function getVote(app: FastifyInstance) {
+  app.get("/polls/:pollId/vote", async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = getPollParams.parse(request.params);
+
+    const { sessionId } = request.cookies;
+
+    if (!sessionId) {
+      return reply.status(100).send({ pollOptionId: undefined });
+    }
+
+    const pollOptionId = await prisma.vote.findUnique({
+      where: {
+        sessionId_pollId: {
+          pollId,
+          sessionId,
+        },
+      },
+      select: {
+        pollOptionId: true,
+      },
+    });
+
+    if (!pollOptionId) {
+      return reply.status(400).send({ message: "Vote not found." });
+    }
+
+    return reply.send({
+      pollOptionId,
+    });
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,7 @@ import cookie from "@fastify/cookie";
 import websocket from "@fastify/websocket";
 import { pollResults } from "./ws/poll-results";
 import fastifyCors from "@fastify/cors";
+import { getVote } from "./routes/get-vote";
 
 const app = fastify();
 
@@ -24,6 +25,7 @@ app.register(websocket);
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(getVote);
 
 app.register(pollResults);
 
